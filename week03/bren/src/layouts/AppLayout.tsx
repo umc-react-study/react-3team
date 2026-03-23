4d6ebefe0ef322df76ef53024c90f3a641ec0ff8
bsh-ko <ssuh3872@gmail.com>
2026-03-23 12:47:37 +0900

    style: 레이아웃 배경색 변경

--- week03/bren/src/layouts/AppLayout.tsx
+++ week03/bren/src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import Header from "../components/common/Header";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-slate-100">
+    <div className="min-h-screen bg-[#222]">
       <Header />
       <main className="mx-auto max-w-6xl px-4 py-8">
         <Outlet />
